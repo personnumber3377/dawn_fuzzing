e0b5b78c83e7e7745a3b73eca5ae99c0ffef0ad7
root <sontapaa.jokulainen@gmail.com>
2026-02-01 21:25:01 +0200

    Blah

--- darthshader_lib/build.rs
+++ darthshader_lib/build.rs
@@ -3,15 +3,26 @@ use std::path::PathBuf;
 fn build_wgsl() {
     let dir: PathBuf = ["tree-sitter-wgsl", "src"].iter().collect();
 
-    println!("cargo:rerun-if-changed={}", dir.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}", dir.display());
 
-    cc::Build::new()
+    let mut build = cc::Build::new();
+
+    build
+        .cpp(true)                    // IMPORTANT: C++
+        .compiler("clang++")           // IMPORTANT
         .include(&dir)
         .file(dir.join("parser.c"))
         .file(dir.join("scanner.cc"))
+        .flag("-stdlib=libc++")        // IMPORTANT
+        .flag("-fPIC")
         .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .compile("tree-sitter-wgsl");
+        .flag_if_supported("-Wno-unused-but-set-variable");
+
+    build.compile("tree-sitter-wgsl");
+
+    // Tell Rust to link libc++
+    println!("cargo:rustc-link-lib=c++");
+    println!("cargo:rustc-link-lib=c++abi");
 }
 
 fn main() {
